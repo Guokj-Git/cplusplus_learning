9a1d8fb9675b3962f2f15d091d55118741610d26
HzKwok <w571374302@live.com>
2019-03-30 09:25:56 +0800

    git push method

---
+++ c++_learning/git_push_method.h
@@ -0,0 +1,13 @@
+/*
+创建, 合并与删除分支
+	git branch //create a new branch
+	git branch -d <name> //delete the branch
+	git checkout <name> //switched to the branch <name>
+	git checkout -b <name> //create and switched to new branch
+	git merge <name> //merge the <name> to current branch
+提交修改( 注意此操纵仅涉及所处的分支上 )
+	git add <filename>
+	git commit -m "label"
+同步到Github
+	git push origin master
+*/
