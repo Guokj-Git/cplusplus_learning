e7dc9c68551a829b8422a9f2d7417beebed0c014
HzKwok <w571374302@live.com>
2019-03-30 10:50:32 +0800

    7.1.2

--- c++_learning/chap7_class.h
+++ c++_learning/chap7_class.h
@@ -3,7 +3,32 @@
 
 //7.1 定义抽象数据类型
 
-struct data
-{
-
+struct Sales_data{
+	//关于Sales_data对象的操作
+	std::string isbn() const { return bookNo; }//1)隐式形参：指向调用该函数的对象，但在类中不明示。
+											   //	return bookNo 相当于 return this->bookNo, this 为隐式形参。
+											   //2)常量成员函数：在参数列表后添加const, 表示将隐式形参声明为指向常量的指针，
+											   //	因而isbn()不能改变调用它的对象的内容。
+	Sales_data& combine(const Sales_data&);
+	double avg_price() const;
+	//数据成员
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
 };
+//在类外部定义成员函数
+double Sales_data::avg_price() const{
+	if (units_sold)
+		return revenue / units_sold;
+	else
+		return 0;
+}
+Sales_data& Sales_data::combine(const Sales_data &rhs) {
+	units_sold += rhs.units_sold;
+	revenue += rhs.revenue;
+	return *this;
+}
+//Sales_data的非成员函数
+Sales_data add(const Sales_data&, const Sales_data&);
+std::ostream &print(std::ostream&, const Sales_data&);
+std::istream &read(std::istream&, Sales_data);
